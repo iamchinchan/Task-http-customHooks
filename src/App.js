4d7e72521719d707eef9bf5bc79bbf6c1ecd4340
Jatin Goyal <goyaljatin344@gmail.com>
2021-11-30 16:41:51 +0530

    created custom hooks and added to app.js for fetching/getting data from firebase realtime database

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
-import React, { useEffect, useState } from 'react';
-
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import React, { useEffect, useState } from "react";
+import useHttp from "./customHooks/useHttp";
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
+  const fetchTaskHandler = (taskData) => {
+    const loadedTasks = [];
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in taskData) {
+      loadedTasks.push({ id: taskKey, text: taskData[taskKey].text });
     }
-    setIsLoading(false);
+    setTasks(loadedTasks);
   };
+  const {isLoading,error,sendRequest:fetchTasks} = useHttp(
+    { url: "https://react-https-bfa5d-default-rtdb.firebaseio.com/tasks.json" },
+    fetchTaskHandler
+  );
 
   useEffect(() => {
     fetchTasks();
@@ -47,7 +30,7 @@ function App() {
     <React.Fragment>
       <NewTask onAddTask={taskAddHandler} />
       <Tasks
-        items={tasks}
+        items={tasks} 
         loading={isLoading}
         error={error}
         onFetch={fetchTasks}
